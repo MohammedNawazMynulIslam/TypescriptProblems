af5045681a68a8210a6ce6245de7db50f11abf03
MohammedNawazMynulIslam <mynulislam183@gmail.com>
2026-05-06 23:32:08 +0600

    completed problem 3

--- src/solutions.ts
+++ src/solutions.ts
@@ -1,16 +1,16 @@
-type ArrayOfRandomNumbers = number
+type ArrayOfRandomNumbers = number;
 
 function filterEvenNumbers(numbers: ArrayOfRandomNumbers[]) {
- 
   return numbers.filter((num) => num % 2 === 0);
 }
 
-
-
-
-type ReverseTheString = string
-
-function reverseString(GiveAInput: ReverseTheString) { 
+type ReverseTheString = string;
+function reverseString(GiveAInput: ReverseTheString) {
   return GiveAInput.split("").reverse().join("");
 }
 
+type StringOrNumber = string | number;
+
+function checkType(InputChecker: StringOrNumber) {
+  return typeof InputChecker === "string" ? "String" : "Number";
+}
